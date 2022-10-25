4bf96da0b7775b9a50c33b511cfd1e9b57cf8a9c
JohanSannier <johan.sanniercaffier@gmail.com>
2022-10-25 14:44:39 +0200

    tmp abouts

---
+++ src/constants/abouts.js
@@ -0,0 +1,24 @@
+import { images } from "./index";
+
+export const abouts = [
+  {
+    title: "Développement web",
+    description: "Je suis un bon développeur",
+    imgUrl: images.about01,
+  },
+  {
+    title: "MERN Stack",
+    description: "Je suis un bon développeur",
+    imgUrl: images.about02,
+  },
+  {
+    title: "UI / UX",
+    description: "Je suis un bon développeur",
+    imgUrl: images.about03,
+  },
+  {
+    title: "React / Redux Apps",
+    description: "Je suis un bon développeur",
+    imgUrl: images.about04,
+  },
+];
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { abouts } from "../../constants/abouts.js";
+
 import "./About.scss";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className="head-text">
+        Je sais qu'une <span>Bonne App</span>
+        <br /> est synonyme d'un <span>Travail Apprécié</span>
+      </h2>
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            key={`${about.title}${index}`}
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
