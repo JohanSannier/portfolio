04fb0a7ff71c077e1a086d64bd4a530fb2a0004a
JohanSannier <johan.sanniercaffier@gmail.com>
2022-10-25 11:10:47 +0200

    ajout folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonial,
+  Work,
+} from "./container/index";
+import { Navbar } from "./components";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
