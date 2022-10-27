0120ddfaf0f8028d3c77d2f6f790fb7419c980d7
JohanSannier <johan.sanniercaffier@gmail.com>
2022-10-27 13:34:35 +0200

    skills tmp content

---
+++ src/constants/experiences.js
@@ -0,0 +1,27 @@
+export const experiences = [
+  {
+    year: "2020",
+    works: [
+      {
+        name: "Frontend Developer",
+        company: "Google",
+        description: "I worked as a frontend developer at Google",
+      },
+      {
+        name: "Frontend Developer",
+        company: "Google",
+        description: "I worked as a frontend developer at Google",
+      },
+    ],
+  },
+  {
+    year: "2021",
+    works: [
+      {
+        name: "Backend Developer",
+        company: "Amazon",
+        description: "I worked as a backend developer at Google",
+      },
+    ],
+  },
+];
---
+++ src/constants/skills.js
@@ -0,0 +1,13 @@
+import images from "./images";
+
+export const skills = [
+  { name: "HTML", icon: images.html, bgColor: "" },
+  { name: "CSS", icon: images.css, bgColor: "" },
+  { name: "Sass", icon: images.sass, bgColor: "" },
+  { name: "Javascript", icon: images.javascript, bgColor: "" },
+  { name: "React", icon: images.react, bgColor: "" },
+  { name: "Redux", icon: images.redux, bgColor: "" },
+  { name: "Typescript", icon: images.typescript, bgColor: "" },
+  { name: "Git", icon: images.git, bgColor: "" },
+  { name: "Figma", icon: images.figma, bgColor: "" },
+];
